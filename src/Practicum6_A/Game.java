46a0f24efc8cde5902b2c08de28148e6c9e65f04
TacoBelmont <thom.koren@gmail.com>
2022-03-10 11:23:50 +0100

    Code is klaar

---
+++ src/Practicum6_A/Game.java
@@ -0,0 +1,47 @@
+package Practicum6_A;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.time.LocalDate;
+import java.util.Locale;
+
+public class Game {
+    private String naam;
+    private int releaseJaar;
+    private double nieuwprijs;
+    private double prijs;
+    private String prijsString;
+
+    public Game(String nm, int rJ, double newPr){
+        naam = nm; releaseJaar = rJ; nieuwprijs = newPr;
+    }
+    public String getNaam(){
+        return naam;
+    }
+    public double huidigeWaarde(){
+        prijs = nieuwprijs;
+        for (int i = 0; i < (LocalDate.now().getYear() - releaseJaar); i++) {
+            prijs *= 0.7;
+        }
+        prijsString = String.format(Locale.US, "%.2f", prijs);
+        prijs = Double.parseDouble(prijsString);
+        return prijs;
+    }
+
+    public boolean equals(Object andereObject){
+        boolean result = false;
+        if (andereObject instanceof Game){
+            Game testObject = (Game) andereObject;
+            if (this.naam.equals(testObject.naam) &&
+                this.releaseJaar == testObject.releaseJaar &&
+                this.nieuwprijs == testObject.nieuwprijs) {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    public String toString(){
+        return "\n" + this.getNaam() + ", uitgegeven in " + releaseJaar + " nieuwprijs: €"+ nieuwprijs + " nu voor: €" + this.huidigeWaarde();
+    }
+}
---
+++ src/Practicum6_A/Persoon.java
@@ -0,0 +1,50 @@
+package Practicum6_A;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class Persoon {
+    private String naam;
+    private double budget;
+    private String budgetString;
+    private ArrayList<Game> mijnGames = new ArrayList<>();
+
+    public Persoon(String nm, double bud){
+        naam = nm; budget = bud;
+    }
+    public double getBudget(){
+        budgetString = String.format(Locale.US, "%.2f", budget);
+        budget = Double.parseDouble(budgetString);
+        return budget;
+    }
+    public boolean koop(Game g)
+    {
+        if (mijnGames.contains(g) == false && budget >= g.huidigeWaarde())
+        {
+            budget -= g.huidigeWaarde();
+            mijnGames.add(g);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean verkoop(Game g, Persoon koper)
+    {
+        if (mijnGames.contains(g) && koper.budget > g.huidigeWaarde())
+        {
+            koper.budget -= g.huidigeWaarde();
+            this.budget += g.huidigeWaarde();
+            this.mijnGames.remove(g);
+            koper.mijnGames.add(g);
+            return true;
+        }
+        return false;
+    }
+    public String toString(){
+        String result = naam + " heeft een budget van €" + this.getBudget() + " en bezit de volgende games:";
+        for (Game game : mijnGames){
+            result += game.toString();
+        }
+        return result;
+    }
+}
